c9b2b0a12cce04f5fd467f54dce40cfa0e106218
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2023-09-05 21:45:47 -0700

    feat: header and footer core

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,16 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), icon({
+      iconDir: "src/icons",
+      include: {
+        ri: ["*"],
+      },
+    }),
+  ],
 });
