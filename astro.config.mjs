ccebe36709fdad1f6dc6da65ca8508194fd7f135
Shawn Schwartz <shawn.t.schwartz@gmail.com>
2023-09-07 02:37:52 -0700

    fix: site url config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://shawnschwartz.com',
   integrations: [mdx(), sitemap(), tailwind(), icon({
       iconDir: "src/icons",
       include: {
