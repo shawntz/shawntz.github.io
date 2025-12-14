8aff69a79999d2f3461af3efaf71208b12453bd4
shawn@schwartz.so <shawn@schwartz.so>
2025-12-14 01:31:06 -0800

    Add astro-icon integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,21 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://shawnschwartz.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    icon({
+      include: {
+        mdi: ["*"],
+        "simple-icons": ["*"],
+      },
+    }),
+  ],
   markdown: {
     shikiConfig: {
       theme: "github-dark",
