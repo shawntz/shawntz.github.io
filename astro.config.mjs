348c0ed621a3f710184eb22429e7bc38eb75466c
Shawn Schwartz <shawn@schwartz.so>
2025-12-28 12:16:47 -0800

    Add compression plugin to Astro config
    Integrated @playform/compress plugin to enable compression for CSS, HTML, JavaScript, and images (excluding SVG) in the Astro project. This should improve site performance by reducing asset sizes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import compress from '@playform/compress';
 
 export default defineConfig({
   site: "https://shawnschwartz.com",
@@ -16,6 +17,13 @@ export default defineConfig({
         "simple-icons": ["*"],
       },
     }),
+    compress({
+      CSS: true,
+      HTML: true,
+      JavaScript: true,
+      Image: true,
+      SVG: false,
+    })
   ],
   markdown: {
     shikiConfig: {
