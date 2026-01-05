418b0aa39d2b2eac0d629982e34da83eedab3c6f
Shawn Schwartz <shawn@schwartz.so>
2026-01-04 16:11:47 -0800

    Add HTML content to RSS feed items
    Introduced remark and rehype processing to convert blog post content to HTML for RSS feed items. Updated dependencies in package.json to include remark, remark-rehype, and rehype-stringify.

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -1,11 +1,39 @@
 import { getBlogPosts } from "@/lib/mdx";
+import { remark } from "remark";
+import remarkGfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+
+async function mdxToHtml(content: string): Promise<string> {
+  const result = await remark()
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .process(content);
+  return String(result);
+}
 
 export async function GET() {
   const posts = await getBlogPosts();
   const siteUrl = "https://shawnschwartz.com";
 
+  const items = await Promise.all(
+    posts.map(async (post) => {
+      const htmlContent = await mdxToHtml(post.content);
+      return `
+    <item>
+      <title><![CDATA[${post.frontmatter.title}]]></title>
+      <description><![CDATA[${post.frontmatter.description}]]></description>
+      <link>${siteUrl}/blog/${post.slug}</link>
+      <guid isPermaLink="true">${siteUrl}/blog/${post.slug}</guid>
+      <pubDate>${new Date(post.frontmatter.date).toUTCString()}</pubDate>
+      <content:encoded><![CDATA[${htmlContent}]]></content:encoded>
+    </item>`;
+    })
+  );
+
   const rss = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/">
   <channel>
     <title>Shawn Schwartz</title>
     <description>Software engineer and researcher sharing insights on programming, research, and technology.</description>
@@ -13,18 +41,7 @@ export async function GET() {
     <atom:link href="${siteUrl}/feed.xml" rel="self" type="application/rss+xml"/>
     <language>en-US</language>
     <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-    ${posts
-      .map(
-        (post) => `
-    <item>
-      <title><![CDATA[${post.frontmatter.title}]]></title>
-      <description><![CDATA[${post.frontmatter.description}]]></description>
-      <link>${siteUrl}/blog/${post.slug}</link>
-      <guid isPermaLink="true">${siteUrl}/blog/${post.slug}</guid>
-      <pubDate>${new Date(post.frontmatter.date).toUTCString()}</pubDate>
-    </item>`
-      )
-      .join("")}
+    ${items.join("")}
   </channel>
 </rss>`;
 
